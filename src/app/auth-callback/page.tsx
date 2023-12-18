6713f9f79a501b629dff7f4f53be5602b57295bb
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-12-18 17:03:22 +0530

    temp

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,17 @@
+import { useRouter, useSearchParams } from 'next/navigation';
+import { trpc } from '../_trpc/client';
+
+export default function AuthCallback() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const origin = searchParams.get('origin');
+
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        router.push(origin ? `/${origin}` : '/dashboard');
+      }
+    },
+  });
+  return <div>page</div>;
+}
